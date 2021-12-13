163ffe8de511785ab3ef4edfdb3659e0aa70e880
Llewellyn Roos <llewellyn.roos@rapiddeploy.com>
2021-12-13 20:13:07 +0200

    Optimised output

--- Day13/Day13.cs
+++ Day13/Day13.cs
@@ -8,11 +8,7 @@ public class Day13 : ISolution
                 .Select(line => line.Substring(11).Split("="))
                 .Select(fold => (fold[0][0], int.Parse(fold[1])));
 
-        int min(char way)
-        {
-            var steps = folds.Where(fold => fold.Item1 == way);
-            return steps.Any() ? steps.Min(fold => fold.Item2) + 1 : int.MaxValue;
-        }
+        int min(char way) => folds.Where(fold => fold.Item1 == way).Min(fold => fold.Item2) + 1;
         var width = min('x');
         var height = min('y');
 
@@ -22,22 +18,12 @@ public class Day13 : ISolution
 
         var dots = input.TakeWhile(line => !string.IsNullOrEmpty(line))
             .Select(line => line.Split(",").Select(x => int.Parse(x)).ToArray())
-            .Select(xy => (translate(xy[0], width), translate(xy[1], height)))
-            .Where(xy => xy.Item1 > -1 && xy.Item2 > -1)
-            .Distinct();
-
-        var canvas = new char[width, height];
+            .Select(xy => (translate(xy[0], width), translate(xy[1], height)));            
 
         foreach (var dot in dots)
         {
-            canvas[dot.Item1, dot.Item2] = '#';
-        }
-
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width ; x++)
-                Console.Write(canvas[x, y] == 0 ? ' ' : canvas[x, y]);
-            Console.WriteLine();
+            Console.SetCursorPosition(dot.Item1, dot.Item2);
+            Console.Write('#');
         }
 
         return (0, 0);
