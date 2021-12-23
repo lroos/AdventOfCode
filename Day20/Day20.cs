1c00186fec235d70cf8aea860b5634a4f3d9ad09
Llewellyn Roos <llewellyn.roos@rapiddeploy.com>
2021-12-23 18:45:37 +0200

    Day 20 (not working)

---
+++ Day20/Day20.cs
@@ -0,0 +1,48 @@
+﻿using System.Numerics;
+
+public class Day20 : ISolution
+{  
+    record pixel(int x, int y, bool lit);
+
+    public (long, long) Run(string[] input)
+    {
+        var index = input.First();
+        var image = input.Skip(2)
+            .SelectMany((line, y) => line.Select((c, x) => new pixel(x, y, c == '#')))
+            .ToArray();
+        int min = 0, max = input.Skip(2).Count() - 1;
+
+        IEnumerable<pixel> zoom(int x, int y)
+            => image.Where(p => Math.Abs(x - p.x) < 2 && Math.Abs(y - p.y) < 2);
+
+        IEnumerable<pixel> getMask(int min, int max)
+            => from y in Enumerable.Range(min - 1, max - min + 3)
+               from x in Enumerable.Range(min - 1, max - min + 3)
+               where x <= min || x >= max || y <= min || y >= max
+               select new pixel(x, y, false);
+
+        for (var step = 0; step < 2; step++)
+        {
+            image = image.Union(getMask(--min, ++max)).ToArray();
+
+            image = (from y in Enumerable.Range(min, max - min + 1)
+                     from x in Enumerable.Range(min, max - min + 1)
+                     let key = Convert.ToInt32(string.Join("", zoom(x, y).Select(p => p.lit ? '1' : '0')), 2)
+                     select new pixel(x, y, index[key] == '#'))
+                       .ToArray();
+
+
+            foreach (var line in image.OrderBy(p => p.y).ThenBy(p => p.x).GroupBy(p => p.y)
+                .Select(g => string.Join("", g.Select(p => p.lit ? "#" : "."))))
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine();
+
+        }
+
+        var lit = image.Count(p => p.lit);
+
+        return (lit, 0);
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ﻿# Advent Of Code 2021 Solutions
 
 ![Stars](https://img.shields.io/badge/stars%38⭐-38-yellow)
-![Days Completed](https://img.shields.io/badge/days%20completed-19-green)
+![Days Completed](https://img.shields.io/badge/days%19completed-19-green)
 
 This repo contains my Advent Of Code 2021 solutions in C# using .NET 6.0.
